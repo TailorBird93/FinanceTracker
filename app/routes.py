b9f8c568b0de6b06b637d6d4df621a1bf8909abb
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-09 17:12:39 +0000

    Modify routes.py to include login and registration routes

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,62 @@
-# from flask import render_template
-# from app import app
-# import os
-
-# @app.route('/')
-# def index():
-#     try:
-#         debug_info = {
-#             "Working directory": os.getcwd(),
-#             "Template folder exists": os.path.exists('templates'),
-#             "Template path": os.path.abspath('templates'),
-#             "Index exists": os.path.exists('templates/index.html')
-#         }
-#         print(debug_info)  # This will show in your console
-#         return render_template('index.html')
-#     except Exception as e:
-#         return f"Error: {str(e)}\nDebug info: {debug_info}"
-
-# @app.route('/test')
-# def test():
-#     return 'This is a test route.'
-
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, TransactionForm
 from app.models import User, Transaction
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
+@app.route('/index')
+@login_required
 def index():
-    transactions = Transaction.query.all()
-    return render_template('index.html', transactions=transactions)
+    form = TransactionForm()
+    if form.validate_on_submit():
+        transaction = Transaction(
+            amount=form.amount.data,
+            category=form.category.data,
+            date=form.date.data,
+            description=form.description.data,
+            user_id=current_user.id
+        )
+        db.session.add(transaction)
+        db.session.commit()
+        flash('Your transaction has been added.')
+        return redirect(url_for('index'))
+    transactions = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.date.desc()).all()
+    return render_template('index.html', title='Home', form=form, transactions=transactions)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
