a141a427d7c014b6e98814f808bec87a660affd8
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-09 16:45:53 +0000

    Update app/__init__.py to initialize LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
 
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
@@ -12,5 +13,7 @@ app = Flask(__name__,
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
