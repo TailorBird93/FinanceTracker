c2b917c9295a6af696bb6194fc9e1962f19324a3
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-09 16:58:19 +0000

    Modify app/models.py to include user authentication methods.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-
-class User(db.Model):
+from flask_login import UserMixin
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
@@ -16,7 +16,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
