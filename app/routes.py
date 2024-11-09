6386370cd152cbabf1dc81a247221e4570829c7a
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-09 16:34:52 +0000

    Change testing and debugging routes to standard

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,30 @@
+# from flask import render_template
+# from app import app
+# import os
+
+# @app.route('/')
+# def index():
+#     try:
+#         debug_info = {
+#             "Working directory": os.getcwd(),
+#             "Template folder exists": os.path.exists('templates'),
+#             "Template path": os.path.abspath('templates'),
+#             "Index exists": os.path.exists('templates/index.html')
+#         }
+#         print(debug_info)  # This will show in your console
+#         return render_template('index.html')
+#     except Exception as e:
+#         return f"Error: {str(e)}\nDebug info: {debug_info}"
+
+# @app.route('/test')
+# def test():
+#     return 'This is a test route.'
+
 from flask import render_template
 from app import app
-import os
+from app.models import User, Transaction
 
 @app.route('/')
 def index():
-    try:
-        debug_info = {
-            "Working directory": os.getcwd(),
-            "Template folder exists": os.path.exists('templates'),
-            "Template path": os.path.abspath('templates'),
-            "Index exists": os.path.exists('templates/index.html')
-        }
-        print(debug_info)  # This will show in your console
-        return render_template('index.html')
-    except Exception as e:
-        return f"Error: {str(e)}\nDebug info: {debug_info}"
-
-@app.route('/test')
-def test():
-    return 'This is a test route.'
+    transactions = Transaction.query.all()
+    return render_template('index.html', transactions=transactions)
