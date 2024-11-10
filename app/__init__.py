8de0b91c2a51bd0faffd879e46702efe58bf21ed
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-10 23:33:06 +0000

    Refactor app structure for consolidated configuration and initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,19 +11,17 @@ login = LoginManager()
 login.login_view = 'login'
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app = Flask(__name__,
+                template_folder='../templates',
+                static_folder='../static')
     app.config.from_object(Config)
 
     
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
     login.init_app(app)
 
-   
     with app.app_context():
-        from app import routes, models
-        
+        from . import routes, models
+
     return app
-
-
-app = create_app()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 
 from flask import render_template, flash, redirect, url_for, request
-from app import app, db
+from app import app,db
 from app.forms import (
     LoginForm,
     RegistrationForm,
