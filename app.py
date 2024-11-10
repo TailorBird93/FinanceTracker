d10d75a47add803d2b4883c36a0defde72275064
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-10 22:44:55 +0000

    Initial commit for Heroku deployment

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from app import app
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-secret-key')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+
+if app.config['SQLALCHEMY_DATABASE_URI'] and app.config['SQLALCHEMY_DATABASE_URI'].startswith('postgres://'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace('postgres://', 'postgresql://', 1)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
