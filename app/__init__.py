2fdff561e816be268860ef67a9531ffa1333a030
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-11 00:02:04 +0000

    Add user_loader for Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from app.models import User
 
 db = SQLAlchemy()
 login = LoginManager()
@@ -17,6 +18,10 @@ def create_app():
     login.init_app(app)
     login.login_view = 'main.login'
 
+    @login.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
     with app.app_context():
         from app.routes import main as main_blueprint  
         app.register_blueprint(main_blueprint)  
