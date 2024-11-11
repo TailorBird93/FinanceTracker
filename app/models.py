ce5adf716e3562f91373a7aae9616bcceace7fc8
Bartosz <99030950+TailorBird93@users.noreply.github.com>
2024-11-11 00:59:46 +0000

    Increase password_hash length to 256

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.Text, nullable=False)
     transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
     categories = db.relationship('Category', backref='user', lazy='dynamic')
 
